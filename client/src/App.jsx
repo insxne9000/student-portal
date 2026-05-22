73b9976792ed33262c4a3768f1e8c54b75dd9c97
redlotus <thedev1ops@gmail.com>
2026-05-22 23:13:38 +0530

    Update App.jsx & login.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,19 @@
-
-import logo from './assets/logo.png'
+import React from "react"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import login from './pages/login'
 
 
 function App() {
   
 
   return (
-    <>
-    <h1>Hello World!</h1>
-      
-    </>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route path='/login' component={login}/>
+
+      </Switch>
+    </Router>
   )
 }
 
