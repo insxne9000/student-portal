ffe6af64a6e40ce61430b008f79b69f36e0654ef
redlotus <thedev1ops@gmail.com>
2026-05-23 09:18:50 +0530

    Update package.json

--- server/server.js
+++ server/server.js
@@ -1,1 +1,29 @@
-console.log('Hello world');
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+import cors from "cors";
+
+dotenv.config('./.env');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });           
